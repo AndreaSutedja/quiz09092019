ac877012b95af08601e0238d8faec0f9f273d8d4
AndreaSutedja <andreasutedja853@gmail.com>
2019-09-13 12:53:55 +0700

    Frist Project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace QUIZ09092019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BangunDatar obj=new BangunDatar();
+
+            obj.LuasPersegi();
+
+            obj.LuasSegitiga();
+
+            obj.LuasLingkaran();
+        }
+
+    }  
+}
